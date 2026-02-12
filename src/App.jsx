b6e16e16ce274ac9e5b895b1aaf0866a1e7c6205
Bhargav reddy <itz.me.bhargav.reddy@gmail.com>
2026-02-12 14:07:36 +0530

    added orders

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router';
 import './App.css'
 import HomePage from './pages/HomePage';
 import CheckoutPage from './pages/CheckoutPage';
+import Orders from './pages/Orders';
 
 function App() {
   
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path='/' element={<HomePage/>} ></Route>
       <Route path='checkout' element={<CheckoutPage/>}  ></Route>
+      <Route path='orders' element={<Orders/>}/>
 
     </Routes>
     
